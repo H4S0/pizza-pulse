c556e19c7f4ebca4238566e1f77472199b187bfd
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-15 15:05:02 +0200

    Update Hero.js

--- src/app/Hero.js
+++ src/app/Hero.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Image from "next/image";
+
 const Hero = () => {
   return (
-    <section>
-      <Image
-        src="../assets/AdobeStock_723625363_Preview.png"
-        alt="alt"
-        width={500}
-        height={550}
-      />
+    <section className="flex items-center justify-between mt-6">
+      <div>
+        <h2 className="text-4xl">
+          Slice into happiness,
+          <br /> one <span className="text-[#973131] font-bold">pizza</span> at
+          a time.
+        </h2>
+        <p className="w-[65vh] mt-5 text-2xl">
+          At ST's PIZZA, we believe that pizza is more than just a meal – it's
+          an experience. Each pizza is a masterpiece, crafted with love and
+          attention to detail, using only the freshest ingredients.
+        </p>
+      </div>
+      <div>
+        <Image src="/pizza.png" alt="alt" width={400} height={450} />
+      </div>
     </section>
   );
 };
