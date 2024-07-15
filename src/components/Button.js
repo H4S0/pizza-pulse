5d9786bba60182f4a5937443ea1b80f4b7ee4acd
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-15 15:04:42 +0200

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Button = ({ children }) => {
-  return <button className="bg-white text-black">{children}</button>;
+  return (
+    <button className="bg-[#973131] text-white rounded-xl py-1 px-2">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
