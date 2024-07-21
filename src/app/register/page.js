12650f2511ed5d7e770fc272eee8727103d34b3e
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-21 20:33:58 +0200

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,8 +1,52 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await fetch("api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (res.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+    setCreatingUser(false);
+  }
   return (
-    <div>
-      <h1>Register Page</h1>
-      {/* Your registration form goes here */}
-    </div>
+    <section>
+      <Navbar />
+      <h2 className="text-center mt-5 text-[#973131] font-semibold">
+        Register
+      </h2>
+      <form className="block max-w-xs mx-auto mt-[10%]" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className="flex gap-4 justify-center">
+          Register
+        </button>
+      </form>
+    </section>
   );
 }
