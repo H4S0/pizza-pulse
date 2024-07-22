3ff4a3277075e7db9db890de8688beb1b2dbf2e6
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-22 12:38:29 +0200

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,23 +1,27 @@
 "use client";
-
 import Navbar from "@/components/Navbar";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
-export default function Register() {
+export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [creatingUser, setCreatingUser] = useState(false);
   const [userCreated, setUserCreated] = useState(false);
   const [error, setError] = useState(false);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const res = await fetch("api/register", {
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
-    if (res.ok) {
+    if (response.ok) {
       setUserCreated(true);
     } else {
       setError(true);
@@ -27,49 +31,65 @@ export default function Register() {
   return (
     <section>
       <Navbar />
-      <h2 className="text-center mt-5 text-[#973131] font-semibold">
-        Register
-      </h2>
-      <form className="max-w-xs mx-auto mt-20" onSubmit={handleSubmit}>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br />
+          Now you can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occurred.
+          <br />
+          Please try again later
+        </div>
+      )}
+      <form
+        className="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto"
+        onSubmit={handleFormSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center text-[#973131]">
+          Register
+        </h2>
         <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
           <input
             type="email"
-            id="email"
             placeholder="Email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            disabled={creatingUser}
+            onChange={(ev) => setEmail(ev.target.value)}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#973131] disabled:bg-gray-200"
           />
         </div>
         <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
           <input
             type="password"
-            id="password"
             placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            disabled={creatingUser}
+            onChange={(ev) => setPassword(ev.target.value)}
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#973131] disabled:bg-gray-200"
           />
         </div>
-        <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        <button
+          type="submit"
+          disabled={creatingUser}
+          className="w-full bg-[#973131] text-white rounded-lg py-2 px-4 transition-all duration-300 ease-in-out hover:scale-105 disabled:opacity-50"
+        >
+          Register
+        </button>
+        <div className="text-center mt-6 text-gray-500 border-t pt-4">
+          Existing account?{" "}
+          <Link
+            className="underline text-[#973131] hover:text-[#761d1d]"
+            href="/login"
           >
-            Register
-          </button>
+            Login here &raquo;
+          </Link>
         </div>
       </form>
     </section>
