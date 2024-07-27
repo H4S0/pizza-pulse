bce977d638e29896e73394b02d32f76d48ef1d97
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-27 12:03:34 +0200

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    username: { type: String, required: true }, //
+    username: { type: String, required: true },
+    admin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
