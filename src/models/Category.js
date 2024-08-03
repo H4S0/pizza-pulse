12b6a678661aa0eff7039066ef12f3a75fc0796f
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-03 19:18:43 +0200

    Update Category.js

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,10 +1,11 @@
-import { model, models, Schema } from "mongoose";
+// models/Category.js
+import mongoose from "mongoose";
 
-const CategorySchema = new Schema(
-  {
-    name: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
 
-export const Category = models?.Category || model("Category", CategorySchema);
+const Category =
+  mongoose.models.Category || mongoose.model("Category", categorySchema);
+
+export { Category };
