4fa76e97612e1e84b5ef22654e926563d56f717b
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-06 13:32:59 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "./Hero";
-import Menu from "./Menu";
+import Menu from "./menu/page";
+
 export default function Home() {
   return (
     <section className="max-w-7xl mx-auto p-4">
       <Navbar />
       <Hero />
-      <Menu/>
+      <Menu />
     </section>
   );
 }
