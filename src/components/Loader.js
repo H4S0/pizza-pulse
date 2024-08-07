ddbb5ddaca16a078bbfe12f73ce5b5ada9e94a71
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-07 15:25:17 +0200

    Add files via upload

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <div class="loader-container">
+      <div class="loader">
+        <div class="inner-circle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
